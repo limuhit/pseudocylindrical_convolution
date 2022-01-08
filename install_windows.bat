12147cee8fbc0405fff0998a36b535112e545542
limuhit <limuhit@gmail.com>
2022-01-08 17:20:28 +0800

    init

---
+++ install_windows.bat
@@ -0,0 +1,5 @@
+chcp 65001
+echo on
+set PATH=%PATH%;D:\Program Files\vs2017\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.25.28610\bin\Hostx64\x64;
+rem change the path to your VS2017 or later
+python setup.py install
